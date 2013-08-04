5bb5ad7ab6b18c059249eaabb19b94a9d74e0ff0
davidkdwyer <davidkdwyer@gmail.com>
2013-08-04 10:37:53 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nantucket::Application.config.secret_token = 'c128540447d35dfc5d88bbd4bf992e0ad80ae5482661413a70f7b68e656c05dc603a573ab205e3c3934054fbeaa2df03dc5b2ced653b73a14bc930671152cb8c'
